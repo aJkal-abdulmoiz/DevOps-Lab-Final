690d926edf6964f32135c5ddb1e6154a62e4a128
abdulmoizdev8333 <abdulmoiziphonexs@gmail.com>
2025-01-16 00:45:07 -0800

    Added React Frontend

---
+++ frontend/src/components/Biography.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Biography = ({imageUrl}) => {
+  return (
+    <>
+      <div className="container biography">
+        <div className="banner">
+          <img src={imageUrl} alt="whoweare" />
+        </div>
+        <div className="banner">
+          <p>Biography</p>
+          <h3>Who We Are</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            blanditiis sequi aperiam. Debitis fugiat harum ex maxime illo
+            consequatur mollitia voluptatem omnis nihil nesciunt beatae esse
+          </p>
+          <p>We are all in 2024!</p>
+          <p>We are working on a MERN STACK PROJECT.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            assumenda exercitationem accusamus sit repellendus quo optio dolorum
+          </p>
+          <p>Lorem ipsum dolor sit amet!</p>
+          <p>Coding is fun!</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Biography;
